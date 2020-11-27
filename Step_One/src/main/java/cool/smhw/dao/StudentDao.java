7b3c83fb61369b5bde5f1d5d34d231ff592d9096
Sam <1791694111@qq.com>
2020-11-27 20:43:43 +0800

    try to connect with remote repository

--- Step_One/src/main/java/cool/smhw/dao/StudentDao.java
+++ Step_One/src/main/java/cool/smhw/dao/StudentDao.java
@@ -1,4 +1,11 @@
 package cool.smhw.dao;
 
+import cool.smhw.entity.Student;
+
+import java.util.List;
+
 public interface StudentDao {
+    //查询Student表的所有数据
+    public List<Student> selectStudents();//对应一个sql的执行
+
 }
